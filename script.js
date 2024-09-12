85257660b0a4b888d964408320578d251a7e86a8
LuchockDos <luciano123benitez@gmail.com>
2024-09-12 17:39:30 -0300

    pokemon

---
+++ script.js
@@ -0,0 +1,26 @@
+let url = "https://pokeapi.co/api/v2/pokemon/";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    crearCard(data.results)
+  });
+
+const container = document.getElementById("container")
+
+function crearCard(data) {
+  let cardsHTML = ""
+
+  for (let i = 0; i < data.length; i++) {
+    let user = data[i]
+
+    cardsHTML += `
+    <div class="caja">
+        <p>${user.name}</p>
+        <p>${user.url}</p>
+    </div>
+  `
+
+
+  container.innerHTML = cardsHTML
+}}
